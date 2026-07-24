0e9e6e817d43d7cdb65c8453545024b5d892746b
ZinexDa <zinex521@gmail.com>
2026-07-25 01:31:09 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
@@ -12,7 +10,7 @@ const config = defineConfig({
   plugins: [
     devtools(),
     nitro({ 
-      preset: 'cloudflare-pages',
+      preset: 'vercel',
       rollupConfig: { external: [/^@sentry\//] } 
     }),
     tailwindcss(),
